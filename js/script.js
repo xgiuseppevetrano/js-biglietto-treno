0af54a8013b0d201d7451a06275f577f008c8f19
xgiuseppevetrano <xgiuseppevetrano@gmail.com>
2022-04-29 10:20:53 +0200

    edit post corrections

--- js/script.js
+++ js/script.js
@@ -9,17 +9,13 @@ const age = Number(prompt("Quanti anni ha il passeggero?"));
 //     - va applicato uno sconto del 20% per i minorenni
 //     - va applicato uno sconto del 40% per gli over 65
 //     - l'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo)
-const ticketPrice = Number(kilometers * 0.21);
-const ticketPriceUnder18 = Number(ticketPrice * 0.2);
-const ticketPriceOver65 = Number(ticketPrice * 0.4);
+let ticketPrice = (kilometers * 0.21);
 
 if ( age < 18 ) {
-    console.log(ticketPriceUnder18.toFixed(2));
-    document.getElementById('ticket-price').innerHTML = `${ticketPriceUnder18.toFixed(2)} €`;
+    ticketPrice = (ticketPrice * 0.8);
 } else if ( age >= 65 ) {
-    console.log(ticketPriceOver65.toFixed(2));
-    document.getElementById('ticket-price').innerHTML = `${ticketPriceOver65.toFixed(2)} €`;
-} else {
-    console.log(ticketPrice.toFixed(2));
-    document.getElementById('ticket-price').innerHTML = `${ticketPrice.toFixed(2)} €`;
-}
+    ticketPrice = (ticketPrice * 0.6);
+} 
+
+console.log(ticketPrice.toFixed(2));
+document.getElementById('ticket-price').innerHTML = `${ticketPrice.toFixed(2)} €`;
